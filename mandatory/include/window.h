d34785c39de89b0a08349f613960d5e59a7bb23c
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-01-31 10:19:01 -0300

    refactor: use XK_Escape

--- mandatory/include/window.h
+++ mandatory/include/window.h
@@ -6,17 +6,15 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 18:59:47 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/01/28 10:53:35 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/01/31 10:17:20 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_H
 # define WINDOW_H
 
-# define WINDOW_NAME	"cub3D"
-# define WINDOW_WIDTH	1280
-# define WINDOW_HEIGHT	720
-
-# define X_KEY_ESC 65307
+# define WINDOW_NAME "cub3D"
+# define WINDOW_WIDTH 1280
+# define WINDOW_HEIGHT 720
 
 #endif
--- mandatory/src/handle_events.c
+++ mandatory/src/handle_events.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 10:49:22 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/01/28 10:51:44 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/01/31 10:17:25 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	handle_keypress(int keycode, t_game *game)
 {
-	if (keycode == X_KEY_ESC)
+	if (keycode == XK_Escape)
 		mlx_loop_end(game->mlx);
 	printf("keypress: keycode %d\n", keycode);
 	return (1);
@@ -23,7 +23,7 @@ int	handle_keypress(int keycode, t_game *game)
 
 int	handle_keyrelease(int keycode, t_game *game)
 {
-	if (keycode == X_KEY_ESC)
+	if (keycode == XK_Escape)
 		mlx_loop_end(game->mlx);
 	printf("keyrelease: keycode %d\n", keycode);
 	return (1);
