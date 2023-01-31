377193c6a701c2493243f5dd9c7c28609523ed3c
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-01-31 09:20:56 -0300

    refactor: check errors in mlx

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 22:05:05 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/01/30 16:52:27 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/01/31 07:43:24 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,23 @@
 
 int	game_setup(t_game *game)
 {
+	game->mlx = NULL;
+	game->win = NULL;
 	game->mlx = mlx_init();
+	if (!game->mlx)
+		return (MLX_ERROR);
 	game->win = mlx_new_window(game->mlx,
 			WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
+	if (!game->win)
+		return (MLX_ERROR);
 	mlx_clear_window(game->mlx, game->win);
 	return (0);
 }
 
 int	game_loop(t_game *game)
 {
+	if (!game->mlx || !game->win)
+		return (MLX_ERROR);
 	mlx_hook(game->win, KeyPress, KeyPressMask, &handle_keypress, game);
 	mlx_hook(game->win, KeyRelease, KeyReleaseMask, &handle_keyrelease, game);
 	mlx_hook(game->win, DestroyNotify, NoEventMask, &mlx_loop_end, game->mlx);
@@ -34,9 +42,13 @@ int	game_loop(t_game *game)
 
 int	finish_game(t_game *game)
 {
-	mlx_destroy_window(game->mlx, game->win);
-	mlx_destroy_display(game->mlx);
-	free(game->mlx);
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
 	return (0);
 }
 
