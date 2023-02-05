f68ad3c719c3c48bf6c61b5da41c9df7cedfae19
ichmi <git.ichmi@gmail.com>
2023-02-05 16:02:04 -0300

    add: utils

---
+++ mandatory/src/utils/utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frosa-ma <frosa-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 14:33:15 by frosa-ma          #+#    #+#             */
+/*   Updated: 2023/02/05 16:01:48 by frosa-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	is_reserved_ch(char c, const char *set)
+{
+	while (*set)
+		if (c == *set++)
+			return (1);
+	return (0);
+}
+
+int	perr(const char *str)
+{
+	if (str)
+		printf("%s\n", str);
+	return (0);
+}
