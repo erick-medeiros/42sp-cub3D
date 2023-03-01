7615d6e5ee6157899b6ad81764663eea5265aedb
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-01 15:40:09 -0300

    test: add fps_meter

--- mandatory/include/debug.h
+++ mandatory/include/debug.h
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 12:09:29 by eandre-f          #+#    #+#             */
-/*   Updated: 2023/02/28 16:57:55 by eandre-f         ###   ########.fr       */
+/*   Updated: 2023/03/01 15:36:50 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	print_bits(char *bytes, size_t num_bytes);
 void	print_bits_nl(char *bytes, size_t num_bytes);
 void	debug_vector(char *name, t_vector vector);
 void	debug_engine(t_engine *engine);
+void	fps_meter(void *mlx, int max_frame);
 
 #endif
