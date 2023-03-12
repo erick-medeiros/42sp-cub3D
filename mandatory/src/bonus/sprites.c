9c5f148c9742878f3a6bff2b25964390e0785928
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-03-11 23:31:55 -0300

    WIP: sprite from file

--- mandatory/src/bonus/sprites.c
+++ mandatory/src/bonus/sprites.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sprites.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frosa-ma <frosa-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 22:56:07 by frosa-ma          #+#    #+#             */
-/*   Updated: 2023/03/11 22:56:26 by frosa-ma         ###   ########.fr       */
+/*   Updated: 2023/03/11 23:14:33 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,24 @@ int	check_animated_texture(t_game *game, char *row)
 		game->sprites.textures[++j] = ft_strtrim(tokens[i], " \n\t");
 	ft_free_matrix(tokens);
 	return (1);
+}
+
+t_img	*get_sprite_texture(t_sprite *sprite)
+{
+	t_img	*texture;
+
+	texture = sprite->textures[sprite->num_texture];
+	if (!texture)
+	{
+		sprite->num_texture = 0;
+		return (get_sprite_texture(sprite));
+	}
+	if (sprite->frames == FRAMES_PER_SPRITE)
+	{
+		sprite->num_texture++;
+		sprite->frames = 0;
+	}
+	else
+		sprite->frames++;
+	return (texture);
 }
